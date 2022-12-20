a076dbbe92a11b207c3576828302a0cdbc3d5619
rezasadeqzadeh <reza.sadeqzadeh@gmail.com>
2022-12-20 08:41:26 +0330

    enhancement

--- pull/main.go
+++ pull/main.go
@@ -4,12 +4,16 @@ import (
 	"bufio"
 	"context"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"math/big"
 	"os"
+	"strconv"
+	"strings"
 	"time"
 
 	"github.com/ethereum/go-ethereum"
+	"github.com/ethereum/go-ethereum/accounts/abi"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/ethclient"
 )
@@ -23,6 +27,17 @@ func pullEvent() {
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	abi721File, err := os.Open("ERC712-ABI.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+	abi721Content, err := ioutil.ReadAll(abi721File)
+	if err != nil {
+		log.Fatal(err)
+	}
+	abi721, err := abi.JSON(strings.NewReader(string(abi721Content)))
+
 	var contractAddresses []common.Address
 
 	addressFile, err := os.Open("contracts.txt")
@@ -37,9 +52,13 @@ func pullEvent() {
 
 	// Keck256 Topic : 0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef
 	//Transfer (index_topic_1 address from, index_topic_2 address to, index_topic_3 uint256 tokenId)
-	topic := common.HexToHash("0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef")
+	transferTopic := common.HexToHash("0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef")
+
+	//TransferSingle (index_topic_1 address operator, index_topic_2 address from, index_topic_3 address to, uint256 id, uint256 value)
+	transferSingleTopic := common.HexToHash("0xc3d58168c5ae7397731d063d5bbf3d657854427343f4c083240f7aacaa2d0f62")
+
 	topics := [][]common.Hash{
-		{topic},
+		{transferTopic, transferSingleTopic},
 	}
 
 	lastFetchedBlock := 0
@@ -68,9 +87,41 @@ func pullEvent() {
 		}
 
 		for _, vLog := range logs {
-			fmt.Printf("Block: %d Contract: %s TxHash: %s \n", vLog.BlockNumber, vLog.Address, vLog.TxHash)
+			if len(vLog.Topics) != 4 { //is not ERC721
+				continue
+			}
+			funcSignature := common.HexToAddress(vLog.Topics[0].Hex())
+			var from, to common.Address
+			var tokenID int64
+
+			switch funcSignature.Hex() {
+			case transferTopic.Hex():
+				from = common.HexToAddress(vLog.Topics[1].Hex())
+				to = common.HexToAddress(vLog.Topics[2].Hex())
+				tokenID, err = HexToInt64(vLog.Topics[3].Hex())
+			case transferSingleTopic.Hex():
+				from = common.HexToAddress(vLog.Topics[2].Hex())
+				to = common.HexToAddress(vLog.Topics[3].Hex())
+				tokenInfo := struct {
+					ID    int64 `json:"id"`
+					Value int64 `json:"value"`
+				}{}
+				abi721.UnpackIntoInterface(&tokenInfo, "TransferSingle", vLog.Data)
+				tokenID = tokenInfo.ID
+			}
+
+			if err != nil {
+				fmt.Println(err)
+			}
+			fmt.Printf("Block: %d Contract: %s TxHash: %s Frm: %s To: %s TokenId: %d \n", vLog.BlockNumber, vLog.Address, vLog.TxHash, from, to, tokenID)
 		}
 		lastFetchedBlock = int(currentBlock) + 1
 		time.Sleep(10 * time.Second)
 	}
+}
+
+func HexToInt64(hex string) (int64, error) {
+	hex = strings.Replace(hex, "0x", "", -1)
+	hex = strings.Replace(hex, "0X", "", -1)
+	return strconv.ParseInt(hex, 16, 64)
 }
